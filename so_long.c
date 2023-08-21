aeb624220f618807032d7e333adc46e9c65b06e7
吉田和司 <yoshidakazushi@MacBook-Pro-7.local>
2023-08-21 13:18:34 +0900

    syusei:

--- so_long.c
+++ so_long.c
@@ -6,19 +6,19 @@
 /*   By: yoshidakazushi <yoshidakazushi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 11:15:18 by yoshidakazu       #+#    #+#             */
-/*   Updated: 2023/08/21 12:27:26 by yoshidakazu      ###   ########.fr       */
+/*   Updated: 2023/08/21 13:18:20 by yoshidakazu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	closes(int keycode, t_game *vars)
+int	closes(int keycode, t_game *game)
 {
-	if (keycode || vars)
+	if (keycode || game)
 		;
 	printf("closed\n");
 	exit(EXIT_SUCCESS);
-	mlx_destroy_window(vars->mlx, vars->window);
+	mlx_destroy_window(game->mlx, game->window);
 	return (0);
 }
 
@@ -28,7 +28,7 @@ int	main(int argc, char *argv[])
 	game = (t_game *)malloc(sizeof(t_game));
 	if (!game)
 	{
-		perror("----malloc_error------\n");
+		perror("ERROR\nmalloc_error\n");
 		return (FALSE);
 	}
 	game->map.movement = 0;
@@ -42,7 +42,7 @@ int	main(int argc, char *argv[])
 	mlx_key_hook(game->window, what_key, game);
 	mlx_hook(game->window, DELETE_EVE, DELETE_MASK, closes, game);
 	mlx_loop(game->mlx);
-	return (0);
+	return (TRUE);
 }
 __attribute__((destructor)) // leakがないかチェック
 static void
